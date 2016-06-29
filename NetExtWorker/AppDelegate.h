362e0e0102cb10258f42bb2087ea57fc64c0b799
Xuan Liu <xuanliu@xuanliu.lan.appannie.com>
2016-06-29 17:43:01 +0800

    initial commit

---
+++ NetExtWorker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetExtWorker
+//
+//  Created by Xuan Liu on 6/29/16.
+//  Copyright © 2016 App Annie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
